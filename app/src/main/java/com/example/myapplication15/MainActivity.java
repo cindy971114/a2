734912ec1343de626f9501554bfa436697df093d
GetScholarship <cindy971114@naver.com>
2020-04-10 19:02:47 +0900

    n

--- app/src/main/java/com/example/myapplication15/MainActivity.java
+++ app/src/main/java/com/example/myapplication15/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_menu, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_menu, R.id.navigation_favorite)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
